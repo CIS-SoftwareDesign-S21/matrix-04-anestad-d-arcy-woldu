40a646df81c2ad0dcd234042db1aeebf55e3fa48
Michael J D'Arcy <tug16338@chinook.cis.temple.edu>
2021-03-17 13:51:23 -0400

    added file implementation

--- create_data.c
+++ create_data.c
@@ -6,8 +6,9 @@
 #include "mat.h"
 
 double calcTime(int (*fun_ptr)(double *c, double *a, int aRows, int aCols, double *b, int bRows, int bCols), double *a, double *b, int n);
+void print_matrix_to_file(FILE *ptr, double *a, double *b, int n);
 
-int main(int argc){
+int main(int argc, char* argv[]){
   struct timespec start;
   struct timespec end;
   struct timespec res;
@@ -17,24 +18,33 @@ int main(int argc){
   if((cfPtr = fopen("data.txt", "w")) == NULL){
     puts("File cannot be opened");
   }else{
-    int n[] = {1,2,3,4,5,10,20,50,100,200,300,400,500};
-    for(int i = 0; i < 13; i++){
-      double times[3];
-      a = gen_matrix(n[i], n[i]);
-      b = gen_matrix(n[i], n[i]);
-
-      fprintf(cfPtr, "%d", n[i]);
-
-      fprintf(cfPtr, ", %f", calcTime(mmult,         a, b, n[i]));
-      fprintf(cfPtr, ", %f", calcTime(mmult_simd,    a, b, n[i]));
-      fprintf(cfPtr, ", %f", calcTime(mmult_simd_O3, a, b, n[i]));
-      fprintf(cfPtr, ", %f", calcTime(mmult_omp,     a, b, n[i]));
-      fprintf(cfPtr, ", 0.000000"); //TODO MPI w/o OpenMP
-      fprintf(cfPtr, ", 0.000000"); //TODO MPI w/ OpenMP
-
-      fprintf(cfPtr, "\n");
-    }
+    if (argc == 3) {
+	int n = get_matrix_size_from_file(argv[1]);
+        a = read_matrix_from_file(argv[1]);
+        b = read_matrix_from_file(argv[2]);
+        print_matrix_to_file(cfPtr, a, b, n);
+    } else if (argc != 1) {
+        puts("create_data <file_1> <file_2>");
+    } else {
+    	int n[] = {1,2,3,4,5,10,20,50,100,200,300,400,500};
+    	for(int i = 0; i < 13; i++){
+      	  a = gen_matrix(n[i], n[i]);
+      	  b = gen_matrix(n[i], n[i]);
+      	  print_matrix_to_file(cfPtr, a, b, n[i]);
+    	}
+     }
   }
+}
+
+void print_matrix_to_file(FILE *ptr, double *a, double *b, int n) {
+      fprintf(ptr, "%d", n);
+      fprintf(ptr, ", %f", calcTime(mmult,         a, b, n));
+      fprintf(ptr, ", %f", calcTime(mmult_simd,    a, b, n));
+      fprintf(ptr, ", %f", calcTime(mmult_simd_O3, a, b, n));
+      fprintf(ptr, ", %f", calcTime(mmult_omp,     a, b, n));
+      fprintf(ptr, ", 0.000000"); //TODO MPI w/o OpenMP
+      fprintf(ptr, ", 0.000000"); //TODO MPI w/ OpenMP
+      fprintf(ptr, "\n");
 }
 
 
